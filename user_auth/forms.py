9e2be5a0af55afae8060f9016011f248a156edfb
Shaikh Saad Ali <109274606+SkSaadAli@users.noreply.github.com>
2023-08-24 20:27:14 +0530

    Update forms.py

--- user_auth/forms.py
+++ user_auth/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from .models import User, Blog
+from .models import User, Blog, Appointment
+# from datetime import date, datetime, time, timedelta
+
 
 class UserSignupForm(forms.ModelForm):
     password_confirm = forms.CharField(widget=forms.PasswordInput)
@@ -52,3 +54,65 @@ class BlogForm(forms.ModelForm):
         fields = [ 'category', 'name', 'blog_picture', 'content','draft']
 
 
+
+
+
+
+from .models import Appointment
+# from datetime import time, timedelta, datetime
+import datetime
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Appointment
+        fields = ['chosen_date', 'chosen_time','required_speciality']
+
+    def __init__(self, doctor, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.doctor = doctor
+        # self.fields['chosen_date'].widget.attrs['readonly'] = True  # Make the date field readonly
+        # self.fields['chosen_date'].widget = forms.DateInput(attrs={'type': 'date'})
+        self.fields['chosen_date'].widget = forms.DateInput(attrs={'type': 'date', 'id': 'id_chosen_date'})
+
+        self.fields['chosen_time'].widget = forms.Select(choices=self.get_available_slots())
+
+    def get_available_slots(self):
+        chosen_date = self.data.get('chosen_date') or datetime.date.today()
+
+        # Generate all slots for the working hours (9 am to 10 pm) with a 45-minute interval
+        available_slots = []
+        current_time = datetime.time(9, 0)  # Start with 9:00 AM
+        end_time = datetime.time(22, 0)    # End at 10:00 PM
+
+        while current_time < end_time:
+            # available_slots.append(current_time)
+            # current_time = datetime.datetime.combine(datetime.date.today(), current_time)
+            # current_time += datetime.timedelta(minutes=45)
+            # current_time = current_time.time()
+
+
+            current_datetime = datetime.datetime.combine(datetime.date.today(), current_time)
+            # slot = current_time  # Format as "9:00:00"
+            available_slots.append(current_time)
+    
+            # Increment by timedelta
+            current_datetime += datetime.timedelta(minutes=45)
+            
+            # Extract the time component
+            current_time = current_datetime.time()
+            
+
+            if current_datetime.time() >= end_time:
+                break
+
+
+        # Fetch booked appointments for the chosen date
+        booked_appointments = Appointment.objects.filter(
+            doctor=self.doctor,
+            chosen_date=chosen_date
+        ).values_list('chosen_time', flat=True)
+
+        # Exclude booked slots from available slots
+        available_slots = [(slot.strftime('%H:%M:%S'), slot.strftime('%H:%M:%S')) for slot in available_slots if slot not in booked_appointments]
+
+        return available_slots
